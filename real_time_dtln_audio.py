3fa64b6245534008ba97954e83d9e5aba40383f9
Hongfei-Niu <83075692+Hongfei-Niu@users.noreply.github.com>
2021-12-10 12:02:51 +0800

    Update real_time_dtln_audio.py

--- real_time_dtln_audio.py
+++ real_time_dtln_audio.py
@@ -29,7 +29,6 @@ import numpy as np
 import sounddevice as sd
 import soundfile as sf
 import tflite_runtime.interpreter as tflite
-import argparse
 import queue
 
 
@@ -40,30 +39,6 @@ def int_or_str(text):
     except ValueError:
         return text
 
-
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    '-l', '--list-devices', action='store_true',
-    help='show list of audio devices and exit')
-args, remaining = parser.parse_known_args()
-if args.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser])
-parser.add_argument(
-    '-i', '--input-device', type=int_or_str,
-    help='input device (numeric ID or substring)')
-parser.add_argument(
-    '-o', '--output-device', type=int_or_str,
-    help='output device (numeric ID or substring)')
-parser.add_argument(
-    '-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
-
-parser.add_argument('--latency', type=float, help='latency in seconds', default=0.2)
-args = parser.parse_args(remaining)
 
 # set some parameters
 block_len_ms = 32
@@ -161,6 +136,7 @@ def removeNoise(data):
     return output
 
 if __name__ == '__main__':
+    # just for test so absolute path is chosen
     try:
         with sf.SoundFile('/Users/wz/PycharmProjects/AI_bot/chat_bot/recording.wav', mode='x', samplerate=16000,
                           channels=1, subtype=args.subtype) as file1:
